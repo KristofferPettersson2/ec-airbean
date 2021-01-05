a06ea314e5b32d9fb4629d3b1533ef4ad742fe79
KristofferPettersson2 <kristoffer.pettersson@utb.ecutbildning.se>
2021-01-05 11:51:56 +0100

    init

---
+++ src/views/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landing">
+    landing
+  <img src="@/assets/graphics/airbean-landing.svg" alt="">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Landing',
+  components: {
+   
+  }
+}
+</script>
